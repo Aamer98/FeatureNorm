c4669ea84d3705b090d09509a7d494ce219e6b32
unknown <ar.aamer@gmail.com>
2021-12-19 16:01:25 +0400

    configs

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 # from .resnet import *
 from .dataparallel_wrapper import *
 from .resnet10 import ResNet10
-from .resnet10_GN import ResNet10_GN
 from .resnet12 import Resnet12
-from .resnet_PixelNorm import resnet18
+from .resnet import resnet18
