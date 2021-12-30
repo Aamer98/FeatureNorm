a8a03d42fc9a3d8d1a1c5161efab7d8824e59ce9
unknown <ar.aamer@gmail.com>
2021-12-31 01:52:39 +0400

    two model methods

--- twoModel_train.py
+++ twoModel_train.py
@@ -552,8 +552,8 @@ def train(model_bn, model_fn, clf_bn, clf_fn,
         logits_base_bn = clf_bn(features_base_bn)    
 
         loss_base_bn = loss_ce(logits_base_bn, y_base)
-        loss_diff = loss_ce(logits_base_bn, logits_base_fn.detach())
-        loss_bn = loss_base_bn + loss_diff
+        #loss_diff = loss_ce(logits_base_bn, logits_base_fn.detach())
+        loss_bn = loss_base_bn
 
 
         print("15")
